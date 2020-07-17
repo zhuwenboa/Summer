fbee2de89a9898744d5aaf5158cd721503002a49
zhuwenboa <879658730@qq.com>
2020-07-17 21:52:43 +0800

    封装socket类

---
+++ src/Socket.cpp
@@ -0,0 +1,55 @@
+#include"Socket.h"
+#include "InetAddress.h"
+#include "SockOps.h"
+
+#include<netinet/in.h>
+#include<netinet/tcp.h>
+#include<stdio.h>
+#include<string.h>
+using namespace Summer;
+
+Socket::~Socket()
+{
+    //关闭连接
+    sockets::close(sockfd_);
+}
+
+void Socket::bindAddress(InetAddress& localaddr)
+{
+    sockets::bindOrDie(sockfd_, localaddr.getSockAddr());
+}
+
+//监听套接字
+void Socket::listen()
+{
+    sockets::listenOrDie(sockfd_);
+}
+
+int Socket::accept(InetAddress* peeraddr)
+{
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    int connfd = sockets::accept(sockfd_, &addr);
+    if(connfd >= 0)
+    {
+        peeraddr->setSockAddr(addr);
+    }   
+    return connfd;
+}
+
+void Socket::setReuseAddr(bool on)
+{
+    int optval = on ? 1 : 0;
+    ::setsockopt(sockfd_, SOL_SOCKET, SO_REUSEADDR, &optval, static_cast<socklen_t>(sizeof(optval)));
+}
+
+void Socket::setReuserPort(bool on)
+{
+    int optval = on ? 1 : 0;
+    int ret = ::setsockopt(sockfd_, SOL_SOCKET, SO_REUSEPORT, &optval, static_cast<socklen_t>(sizeof(optval)));
+    if(ret < 0 && on)
+    {
+        //...
+    }
+}
+
---
+++ src/Socket.h
@@ -0,0 +1,46 @@
+#ifndef SOCKET_H
+#define SOCKET_H
+#include "noncopyable.h"
+#include<netinet/tcp.h>
+struct tcp_info;
+
+namespace Summer{
+class InetAddress;
+
+//封装socket类(封装socket是为了采用RAII的方法，保证对象的生命周期)
+class Socket : noncopyable
+{
+public:  
+    //禁止隐式转换
+    explicit Socket(int sockfd) : sockfd_(sockfd) {}
+
+    ~Socket();
+
+    int fd() const { return sockfd_;}
+
+    bool getTcpInfo(struct tcp_info*) const;
+    bool getTcpInfoString(char* buf, int len) const;
+
+    void bindAddress(InetAddress& localaddr);
+    void listen();
+    int accept(InetAddress* peeraddr);
+    void shutdownWrite();
+
+    void setTcpNoDelay(bool on);
+
+    //设置是否可以重用addr
+    void setReuseAddr(bool on);
+    //设置是否可以重用port
+    void setReuserPort(bool on);
+    //设置套接字是否Alive
+    void setKeepAlive(bool on);
+private:  
+    const int sockfd_;
+};
+
+
+} // namespace Summer;
+
+
+
+#endif
