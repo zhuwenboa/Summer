770030885b645845cae7ed1be9ee19d7049053c7
zhuwenboa <879658730@qq.com>
2020-07-17 21:53:26 +0800

    channel类

---
+++ src/Channel.cpp
@@ -0,0 +1,101 @@
+#include"Channel.h"
+#include "Eventloop.h"
+#include<sys/epoll.h>
+#include<assert.h>
+#include<vector>
+using namespace Summer;
+
+const int Channel::S_ReadEvent = EPOLLIN | EPOLLPRI;
+const int Channel::S_WriteEvent = EPOLLOUT;
+const int Channel::S_NoEvent = 0;
+
+Channel::Channel(Eventloop* loop, int fd)
+    : loop_(loop),
+    fd_(fd),
+    events_(0),
+    Call_events(0),
+    index_(-1),
+    logHup_(true),
+    tied_(false),
+    eventHandling_(false),
+    addToLoop_(false)
+    {}
+
+Channel::~Channel()
+{
+    //析构时保证多路复用基类不再持有channel对象
+    assert(!eventHandling_);
+    assert(!addToLoop_);
+    if(loop_->isInLoopThread())
+    {
+        assert(!loop_->hasChannel(this));
+    }    
+}
+
+void Channel::tie(const std::shared_ptr<void>& obj)
+{
+    tie_ = obj;
+    tied_ = true;
+}
+
+void Channel::update()
+{
+    addToLoop_ = true;
+    loop_->updateChannel(this);
+}
+
+void Channel::reMove()
+{
+    assert(isNoevent());
+    addToLoop_ = false;
+    loop_->removeChannel(this);
+}
+
+void Channel::handleEvent()
+{
+    std::shared_ptr<void> guard;
+    if(tied_)
+    {
+        guard = tie_.lock();
+        if(guard)
+        {
+            handleEventDispenser();
+        }
+    }
+    else
+    {
+        handleEventDispenser();
+    }    
+}
+
+void Channel::handleEventDispenser()
+{
+    eventHandling_ = true;
+    if((Call_events & EPOLLHUP) && !(Call_events & EPOLLIN))
+    {
+        if(logHup_)
+        {
+            //日志
+        }
+        if(closeCallback)
+            closeCallback();
+    }
+
+    if(Call_events & EPOLLERR)
+    {
+        if(erroCallback)
+            erroCallback();
+    }
+    if(Call_events & (EPOLLIN | EPOLLPRI | EPOLLRDHUP))
+    {
+        if(readCallback)
+            readCallback(fd_, loop_);
+    }
+    if(Call_events & EPOLLOUT)
+    {
+        if(writeCallback)
+            writeCallback();
+    }
+    eventHandling_ = false;
+}
+
---
+++ src/Channel.h
@@ -0,0 +1,117 @@
+#ifndef CHANNEL_H
+#define CHANNEL_H
+
+#include "noncopyable.h"
+#include<functional>
+#include<memory>
+
+namespace Summer{
+
+class Eventloop;
+
+class Channel : noncopyable
+{
+public:  
+    typedef std::function<void()> EventCallback;
+    typedef std::function<void(int, Eventloop*)> ReadeventCallback;
+
+    Channel(Eventloop* loop, int fd);
+    ~Channel();
+
+    void handleEvent();
+
+    //设置回调函数
+    void setReadCallback(ReadeventCallback cb)
+    {readCallback = std::move(cb);}
+    void setWriteCallback(EventCallback cb)
+    {writeCallback = std::move(cb);}
+    void setCloseCallback(EventCallback cb)
+    {closeCallback = std::move(cb);}
+    void setErroCallback(EventCallback cb)
+    {erroCallback = std::move(cb);}
+
+    void tie(const std::shared_ptr<void>&);
+
+    int Fd() const 
+    {return fd_;}
+    
+    int event() const
+    {return events_;} 
+
+    void set_Callrevent(int ev)
+    {Call_events = ev;}
+
+    // evnets
+    void enableReading()
+    {
+        events_ |= S_ReadEvent;
+        update();
+    }   
+    void unableReading()
+    {
+        events_ &= ~S_ReadEvent;
+        update();
+    }
+    bool isReading() {return events_ & S_ReadEvent;}
+    void enableWriting()
+    {
+        events_ |= S_WriteEvent;
+        update();   
+    }
+    void unableWriting()
+    {
+        events_ &= ~S_WriteEvent;
+        update();
+    }
+    bool isWriting() { return events_ & S_WriteEvent;}
+    void unableAll()
+    {
+        events_ &= S_NoEvent;
+        update();
+    }
+    bool isNoevent() { return events_ == S_NoEvent;}    
+    //---------
+
+    //for poll/epoll
+    int index() {return index_;}
+    void set_index(int idx) {index_ = idx;}
+
+    //for debug
+    
+
+    Eventloop* ownerLoop() {return loop_;}
+    void reMove();
+private:  
+
+    void update();
+    void handleEventDispenser();
+
+    static const int S_NoEvent;        
+    static const int S_ReadEvent;
+    static const int S_WriteEvent;
+
+    const int fd_;       //套接字描述符
+    Eventloop* loop_;
+    int index_;           //在poll(2)中数组的下标
+    int events_;           //当前channel关注的事件集
+    int Call_events;      //回调事件 用来判断执行什么回调函数
+    bool logHup_;
+
+    std::weak_ptr<void> tie_;
+    bool tied_;
+    bool eventHandling_;
+    bool addToLoop_;
+
+    //四个回调函数
+    ReadeventCallback readCallback;
+    EventCallback writeCallback;
+    EventCallback closeCallback;
+    EventCallback erroCallback;
+
+
+};
+
+
+}
+
+#endif
