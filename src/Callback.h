a4ba72af01a87af1f646d430c515a1231fe108e8
zhuwenboa <879658730@qq.com>
2020-08-14 12:13:30 +0800

    Callback类

--- src/Callback.h
+++ src/Callback.h
@@ -15,7 +15,6 @@ typedef std::function<void (const TcpConnectionPtr&)> ConnectionCallback;
 typedef std::function<void (const TcpConnectionPtr&)> CloseCallback;
 typedef std::function<void (const TcpConnectionPtr&)> WriteCompleteCallback;
 typedef std::function<void (const TcpConnectionPtr&, Buffer*)> MessageCallback;
-typedef std::function<void()> TimerCallback;
 }//namespace Summer;
 
 
